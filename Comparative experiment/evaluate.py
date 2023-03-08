01e753a3a0db7e7df728d6ddfd1a0b4c1bcc6b39
zjtfo <48672420+zjtfo@users.noreply.github.com>
2023-03-08 16:01:58 +0800

    Update evaluate.py

--- Comparative experiment/evaluate.py
+++ Comparative experiment/evaluate.py
@@ -2,20 +2,12 @@ import json
 import os
 
 
-
 def main():
     test_set = json.load(open(os.path.join('./Multi_Teacher_In_Questions/preprocessed_data/', 'count_test.json')))
-    
     res = []
-
     for i in range(len(test_set)):
         answer = test_set[i]['answer']
         res.append(answer)
-        
-        
-        
-        
-
     print(len(res))
 
     pred_fn = './Multi_Teacher_In_Questions/circle_count_encoder_multihop_guide_count_again/output/predict_result_8220/predict.txt'
@@ -23,8 +15,7 @@ def main():
 
     print(len(pred))
 
-
-    
+  
     count = 0
     for i in range(len(test_set)):
         if res[i] == pred[i]:
